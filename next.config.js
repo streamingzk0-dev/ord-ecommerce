9674b8993102c5de39d932dc6d1c69697f783a22
streamingzk0-dev <streamingzk0@gmail.com>
2025-10-15 14:09:30 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,18 @@ const nextConfig = {
   // Netlify Next.js Runtime gère automatiquement le static/dynamic
   images: {
     domains: ['nxpdrunwfztdokqgcxyy.supabase.co'],
+    unoptimized: true
   },
   // Désactiver le strict mode pour éviter les problèmes de build
   reactStrictMode: false,
+  // Pour éviter les warnings
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
