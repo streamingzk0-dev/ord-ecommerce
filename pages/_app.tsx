f4693e58c6ab814c5875f09cb50bf1987b56e58e
streamingzk0-dev <streamingzk0@gmail.com>
2025-10-15 13:14:42 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,16 @@ export default function App({ Component, pageProps }: AppProps) {
     setIsClient(true)
   }, [])
 
+  // Pendant le SSR, afficher un simple loader
   if (!isClient) {
-    return <div>Chargement...</div>
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600 mx-auto"></div>
+          <p className="mt-4 text-gray-600">Chargement...</p>
+        </div>
+      </div>
+    )
   }
 
   return (
